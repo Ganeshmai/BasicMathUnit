794e0e3910556c5929395bef124c548066a8a9fe
Maiganesh G <maiganesh.g@zensar.com>
2022-03-28 11:37:57 +0530

    Add project files.

---
+++ Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathUnit
+{
+   
+    public class Reservation
+    {
+        public User Madeby { get; set; }
+
+        public bool CanbeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+            if(Madeby== user)
+                return true;
+            return false;
+        }
+
+    }
+    public class User {
+        public bool IsAdmin { get; set; }
+    }
+
+}
+
